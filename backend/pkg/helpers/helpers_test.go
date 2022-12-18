77bf1d35c13e036fb3414b4af400c3a744d13c7a
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2022-12-18 19:03:57 +0300

    fix tests

--- backend/pkg/helpers/helpers_test.go
+++ backend/pkg/helpers/helpers_test.go
@@ -1,7 +1,6 @@
 package helpers_test
 
 import (
-	"os"
 	"testing"
 
 	"snaLinkShortener/pkg/helpers"
@@ -11,9 +10,9 @@ import (
 
 func TestGetConfig(t *testing.T) {
 	config := helpers.GetConfig()
-	require.NoError(t, os.Setenv("SHORT_LINK_BASE", "http://localhost:15001/"))
-	require.NoError(t, os.Setenv("ALLOW_CORS_FROM", "localhost:3000"))
-	require.NoError(t, os.Setenv("POSTGRES_CONN_STRING", "postgres://user:password@localhost:5432/linkShortener"))
+	t.Setenv("SHORT_LINK_BASE", "http://localhost:15001/")
+	t.Setenv("ALLOW_CORS_FROM", "localhost:3000")
+	t.Setenv("POSTGRES_CONN_STRING", "postgres://user:password@localhost:5432/linkShortener")
 	require.Equal(t, "localhost:3000", config.CorsAllowFrom)
 	require.Equal(t, "http://localhost:15001/", config.ShortLinkBase)
 	require.Equal(t, "postgres://user:password@localhost:5432/linkShortener", config.DbConnString)
