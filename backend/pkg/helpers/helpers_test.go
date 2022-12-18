47f2f8589d538732987e938a01ba6e8ac5f6acc9
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2022-12-18 19:11:49 +0300

    test

--- backend/pkg/helpers/helpers_test.go
+++ backend/pkg/helpers/helpers_test.go
@@ -13,7 +13,7 @@ func TestGetConfig(t *testing.T) {
 	t.Setenv("SHORT_LINK_BASE", "http://localhost:15001/")
 	t.Setenv("ALLOW_CORS_FROM", "localhost:3000")
 	t.Setenv("POSTGRES_CONN_STRING", "postgres://user:password@localhost:5432/linkShortener")
-	require.Equal(t, "localhost:3000", config.CorsAllowFrom)
 	require.Equal(t, "http://localhost:15001/", config.ShortLinkBase)
 	require.Equal(t, "postgres://user:password@localhost:5432/linkShortener", config.DbConnString)
+	require.Equal(t, "http://localhost:3000", config.CorsAllowFrom)
 }
